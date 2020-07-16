fd4dcf65f1f54a781e0a3e090d68e6a333008ae9
quwinner <quwinner@yande.ru>
2020-07-16 13:02:21 +0400

    Третье задание

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Larareact</title>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    </head>
+    <body>
+        <h1>Hello world!!!</h1>
+        <button onclick="testAjax()">button test</button>
+    </body>
+
+    <script>
+        function testAjax()
+        {
+            axios.get('/test')
+            .then(function (response) {
+            console.log(response.data);
+            })
+            .catch(function (error)
+            {
+                alert('its broken')
+            })
+        }
+    </script>
+</html>
